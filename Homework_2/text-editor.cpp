c9dded4c499a9db2fccf594344b3c3a3bc569e7b
Korsakov Vladislav <KorsakovVDmechanics@yandex.ru>
2025-09-28 19:08:22 +0300

    fix test at HW-2

--- Homework_2/text-editor.cpp
+++ Homework_2/text-editor.cpp
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 using std::string;
 
@@ -28,47 +28,46 @@ int main(int argc, char* argv[]) {
 }
 
 using TE = TextEditor;
-  void TE::print() const {
-    std::cout << left;
-    for (auto it = right.rbegin(); it != right.rend(); ++it) {
-        std::cout << *it;
-    }
-    std::cout << std::endl;
+void TE::print() const {
+  std::cout << left;
+  for (auto it = right.rbegin(); it != right.rend(); ++it) {
+    std::cout << *it;
   }
+  std::cout << std::endl;
+}
 
-  void TE::addText(const string& text) {
-    left += text;
-  }
+void TE::addText(const string& text) {
+  left += text;
+}
 
-  int TE::deleteText(int k) {
-    k = std::min(k, static_cast<int>(left.size()));
-    left.erase(left.end() - k, left.end());
-    return k;
-  }
+int TE::deleteText(int k) {
+  k = std::min(k, static_cast<int>(left.size()));
+  left.erase(left.end() - k, left.end());
+  return k;
+}
 
-  string TE::cursorLeft(int k) {
-    k = std::min(k, static_cast<int>(left.size()));
+string TE::cursorLeft(int k) {
+  k = std::min(k, static_cast<int>(left.size()));
 
-    string move = left.substr(left.size() - k);
-    std::reverse(move.begin(), move.end());
-    right += move;
+  string move = left.substr(left.size() - k);
+  std::reverse(move.begin(), move.end());
+  right += move;
 
-    left.erase(left.end() - k, left.end());
+  left.erase(left.end() - k, left.end());
 
-    return left;
-  }
+  return left;
+}
 
-  string TE::cursorRight(int k) {
-    k = std::min(k, static_cast<int>(right.size()));
-    string move = right.substr(right.size() - k);
-    std::reverse(move.begin(), move.end());
-    left += move;
+string TE::cursorRight(int k) {
+  k = std::min(k, static_cast<int>(right.size()));
+  string move = right.substr(right.size() - k);
+  std::reverse(move.begin(), move.end());
+  left += move;
 
-    right.erase(right.end() - k, right.end());
+  right.erase(right.end() - k, right.end());
 
-    return string(right.rbegin(), right.rend());
-  }
-
+  return string(right.rbegin(), right.rend());
+}
 
 void te_cli(TextEditor& text) {
   text.print();
@@ -78,34 +77,29 @@ void te_cli(TextEditor& text) {
     short in = 0;
     std::cin >> in;
     switch (in) {
-      case 0:{
-        std::cout << "Exit?(1/0)\n";
-        short exit_in = 1;
-        std::cin >> exit_in;
-        if (exit_in) {
-          exit = true;
-        }
+      case 0: {
+        exit = true;
         break;
-	     }
-      case 2:{
-	string str;
-        std::cout << "Enter a text:"; 
-	std::cin >> str;
+      }
+      case 2: {
+        string str;
+        std::cout << "Enter a text:";
+        std::cin >> str;
         std::cout << std::endl;
         text.addText(str);
         break;
-	     }
-      case 3:{
+      }
+      case 3: {
         std::cout << "Enter a number of characters:";
-       	size_t n;
+        size_t n;
         std::cin >> n;
         std::cout << "\n" << text.deleteText(n) << "\n";
         break;
-	     }
-      case 4:{
+      }
+      case 4: {
         while (true) {
           std::cout << "Enter a shift:";
-	  int shift;
+          int shift;
           std::cin >> shift;
           if (shift < 0) {
             text.cursorLeft(-shift);
@@ -116,11 +110,11 @@ void te_cli(TextEditor& text) {
           break;
         }
         break;
-	     }
-      case 5:{
+      }
+      case 5: {
         text.print();
         break;
-	     }
+      }
       case 1:
       default:
         printMenu();
