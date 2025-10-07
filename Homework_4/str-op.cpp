be160e3288796da042cdd3ae7f25fc5ac5ba386b
Korsakov Vladislav <KorsakovVDmechanics@yandex.ru>
2025-10-07 17:07:51 +0300

    google style

--- Homework_4/str-op.cpp
+++ Homework_4/str-op.cpp
@@ -4,41 +4,36 @@
 using std::string;
 
 class String {
-	string str;
-	public:
-	String(const string& str):str(str){}
+  string str;
 
-	class StrProxy {
-		const String& parent;
-		size_t start;
-		public:
-		StrProxy(const String& s, size_t i): parent(s), start(i){}
-		
-		String operator[](size_t end) const {
-			size_t len = end - start;
-			string substr = parent.str.substr(start, len);
-			return String(substr);
-		}	
-	
-	
-	};
+ public:
+  String(const string& str) : str(str) {}
 
-	StrProxy operator[](size_t start) const {
-		return StrProxy(*this, start);
-	}
-	const string& get() const {return str;}
+  class StrProxy {
+    const String& parent;
+    size_t start;
 
+   public:
+    StrProxy(const String& s, size_t i) : parent(s), start(i) {}
+
+    String operator[](size_t end) const {
+      size_t len = end - start;
+      string substr = parent.str.substr(start, len);
+      return String(substr);
+    }
+  };
+
+  StrProxy operator[](size_t start) const { return StrProxy(*this, start); }
+  const string& get() const { return str; }
 };
 
-int main(){
-	String string("hello");
-	std::cout << "hello\n";
-	String substr = string[1][4];
-        std::cout << "substr 'ell':"<< substr.get() << "\n";
-	String str = string[1][2];
-	std::cout << "a char:"<< str.get() << "\n";
-	String empty = string[1][1];
-	std::cout << "empty:" << empty.get() << "\n";
-	
-
+int main() {
+  String string("hello");
+  std::cout << "hello\n";
+  String substr = string[1][4];
+  std::cout << "substr 'ell':" << substr.get() << "\n";
+  String str = string[1][2];
+  std::cout << "a char:" << str.get() << "\n";
+  String empty = string[1][1];
+  std::cout << "empty:" << empty.get() << "\n";
 }
